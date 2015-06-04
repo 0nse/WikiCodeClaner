c4bc43e4cc94f91cdf41f824e521a97c37b618c9
0nse <0nse@users.noreply.github.com>
2015-06-04 14:45:30 +0200

    + Remove dashes as long as they aren't a hyphen connecting two words.

--- removeSymbols.py
+++ removeSymbols.py
@@ -1,3 +1,8 @@
+import re
+# mash all successive dashes (two or more):
+dashes = re.compile(r"-{2,}")
+dashesNotHyphens = re.compile(r"[^\w](-)[^\w]")
+
 def removeSymbols(text):
     """Removes all symbols as they serve no
     semantical purpose for us."""
@@ -8,4 +13,10 @@ def removeSymbols(text):
         # it's faster to determine this first instead of blindly replacing:
         if c in text:
             text = text.replace(c, " ")
+
+    # replace multiple dashes with space:
+    text = dashes.sub(" ", text)
+    # replace leftover dashes that don't act as hyphens connecting two words:
+    text = dashesNotHyphens.sub(" ", text)
+
     return text

25478552b289713289188dae543e053734377972
0nse <0nse@users.noreply.github.com>
2015-06-10 16:59:59 +0200

    / Also make user signature regex case insensitive.

--- signature.py
+++ signature.py
@@ -1,7 +1,7 @@
 import re
 
 # [[User:Any text|Any text]] ([[User talk:Any text|Any text]]) -- the mid bar is optional:
-user = re.compile(r"\[\[User:[^\]]+\]\] \(\[\[User talk:[^\]]+\]\]\)")
+user = re.compile(r"(?i)\[\[User:[^\]]+\]\] \(\[\[User talk:[^\]]+\]\]\)")
 # e.g. 13:41, 9 June 2015 (UTC) -- the characters are the valid characters for all months:
 timestamp = re.compile(r"\d{2}:\d{2}, \d{1,2} (?i)[(January),(February),(March),(April),(May),(June),(July),(August),(October),(November),(December)]+ \d{4} \(\w+\)")
 def removeSignature(text):

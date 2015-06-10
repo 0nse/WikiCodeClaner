ae82fcc83a4fb8e46183a896b1bc34c81ef8a12b
0nse <0nse@users.noreply.github.com>
2015-06-10 10:23:16 +0200

    / Remove apostrophes to create words such as "dont".

--- removeSymbols.py
+++ removeSymbols.py
@@ -9,10 +9,13 @@ def removeSymbols(text):
 
     # string.punctuation also includes "-" which we want to keep.
     # Furthermore, we also want to remove some additional symbols:
-    for c in "<>|[]{}()@#$€£%&*_=+,.?!'\"«»“”;:/\\~`0123456789":
+    for c in "<>|[]{}()@#$€£%&*_=+,.?!\"«»“”;:/\\~`0123456789":
         # it's faster to determine this first instead of blindly replacing:
         if c in text:
             text = text.replace(c, " ")
+    # Remove apostrophes so that we end up with "dont", "wont", "im"
+    # "isnt", "arent", "peters" and so forth:
+    text = text.replace("'", "")
 
     # replace multiple dashes with space:
     text = dashes.sub(" ", text)

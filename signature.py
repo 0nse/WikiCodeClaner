0b0ae041c20b3db68b920e2ef5b6d915329e5a5d
0nse <0nse@users.noreply.github.com>
2015-06-10 16:13:33 +0200

    / Make timestamp regex case insensitive for 0nse/WikiWho. + Increase regex legibility.

--- signature.py
+++ signature.py
@@ -3,8 +3,7 @@ import re
 # [[User:Any text|Any text]] ([[User talk:Any text|Any text]]) -- the mid bar is optional:
 user = re.compile(r"\[\[User:[^\]]+\]\] \(\[\[User talk:[^\]]+\]\]\)")
 # e.g. 13:41, 9 June 2015 (UTC) -- the characters are the valid characters for all months:
-timestamp = re.compile(r"\d{2}:\d{2}, \d{1,2} [JFMASOND][ebruaynchpilgstovm]+ \d{4} \(\w+\)")
-
+timestamp = re.compile(r"\d{2}:\d{2}, \d{1,2} (?i)[(January),(February),(March),(April),(May),(June),(July),(August),(October),(November),(December)]+ \d{4} \(\w+\)")
 def removeSignature(text):
     # @see https://en.wikipedia.org/wiki/Wikipedia:Signatures
     text = user.sub("", text)

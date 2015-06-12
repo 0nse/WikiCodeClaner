8490e676336067145a5d5604b10763524e02bde4
0nse <0nse@users.noreply.github.com>
2015-06-12 12:10:22 +0200

    / Be a bit more liberal with timestamp regex and allow lack of spaces.

--- signature.py
+++ signature.py
@@ -2,8 +2,8 @@ import re
 
 # [[User:Any text|Any text]] ([[User talk:Any text|Any text]]) -- the mid bar is optional:
 user = re.compile(r"(?i)\[\[User:[^\]]+\]\] \(\[\[User talk:[^\]]+\]\]\)")
-# e.g. 13:41, 9 June 2015 (UTC) -- the characters are the valid characters for all months:
-timestamp = re.compile(r"\d{2}:\d{2}, \d{1,2} (?i)[(January),(February),(March),(April),(May),(June),(July),(August),(October),(November),(December)]+ \d{4} \(\w+\)")
+# e.g. 13:41, 9 June 2015 (UTC) -- the spaces after the comma and before the bracket are optional:
+timestamp = re.compile(r"\d{2}:\d{2}, {0,1}\d{1,2} (?i)[(January),(February),(March),(April),(May),(June),(July),(August),(October),(November),(December)]+ \d{4} {0,1}\(\w+\)")
 def removeSignature(text):
     # @see https://en.wikipedia.org/wiki/Wikipedia:Signatures
     text = user.sub("", text)

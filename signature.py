c391faf3df86b93b0eba803ef09a135c952afa48
0nse <0nse@users.noreply.github.com>
2015-08-04 00:24:30 +0200

    / There are some timestamps that abbreviate months with three letters (e.g. Aug for August).

--- signature.py
+++ signature.py
@@ -7,8 +7,8 @@ talkRe = re.compile(r"(?i)" + talk)
 contribsRe = re.compile(r"(?i)\[\[Special:Contributions/[^\]]+\]\]")
 # [[User:Any text|Any text]] talkRe -- the mid bar is optional:
 userRe = re.compile(r"(?i)\[\[User:[^\]]+\]\] \(" + talk + "\)")
-# e.g. 13:41, 9 June 2015 (UTC) -- the spaces after the comma and before the bracket are optional:
-timestampRe = re.compile(r"\d{2}:\d{2}, {0,1}\d{1,2} (?i)[(January),(February),(March),(April),(May),(June),(July),(August),(October),(November),(December)]+ \d{4} {0,1}\(\w+\)")
+# e.g. 13:41, 9 Jun 2015 (UTC) -- the spaces after the comma and before the bracket are optional:
+timestampRe = re.compile(r"\d{2}:\d{2}, {0,1}\d{1,2} (?i)[(Jan(uary)?),(Feb(ruary)?),(Mar(ch)?),(Apr(il)?),(May),(June),(Jul(y)?),(Aug(ust)?),(Oct(ober)?),(Nov(ember)?),(Dec(ember)?)]+ \d{4} {0,1}\(\w+(\)"))?
 
 def removeSignature(text):
     """ Removes different forms of signatures. This includes timestamps,

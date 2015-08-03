9db585b353b2252528c51d468cdaf6c1489875e0
0nse <0nse@users.noreply.github.com>
2015-08-04 01:24:01 +0200

    / Some timestamp re improvements/fixes.

--- signature.py
+++ signature.py
@@ -8,7 +8,9 @@ contribsRe = re.compile(r"(?i)\[\[Special:Contributions/[^\]]+\]\]")
 # [[User:Any text|Any text]] talkRe -- the mid bar is optional:
 userRe = re.compile(r"(?i)\[\[User:[^\]]+\]\] \(" + talk + "\)")
 # e.g. 13:41, 9 Jun 2015 (UTC) -- the spaces after the comma and before the bracket are optional:
-timestampRe = re.compile(r"\d{2}:\d{2}, {0,1}\d{1,2} (?i)[(Jan(uary)?),(Feb(ruary)?),(Mar(ch)?),(Apr(il)?),(May),(Jun(e)?),(Jul(y)?),(Aug(ust)?),(Oct(ober)?),(Nov(ember)?),(Dec(ember)?)]+ \d{4} {0,1}\(\w+\)")
+timestampRe = re.compile(r"\d{2}:\d{2}, ?\d{1,2} (?i)(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Oct(ober)?|Nov(ember)?|Dec(ember)?) \d{4} {0,1}\(\w+\)")
+#                          \________/     \___/  \_______________________________________________________________________________________________________________/ \__/  \__________/
+#                            13:41   , ?    9                                                      Jun                                                             2015      (UTC)
 
 def removeSignature(text):
     """ Removes different forms of signatures. This includes timestamps,

239ec09ac3377fa8392d212026cb57dbaf4a7c8a
0nse <0nse@users.noreply.github.com>
2015-08-10 23:27:11 +0200

    / Be more lenient on timestamps to capture an earlier timestamp format.

--- signature.py
+++ signature.py
@@ -7,10 +7,15 @@ talkRe = re.compile(r"(?i)" + talk)
 contribsRe = re.compile(r"(?i)\[\[Special:Contributions/[^\]]+\]\]")
 # [[User:Any text|Any text]] talkRe -- the mid bar is optional:
 userRe = re.compile(r"(?i)\[\[User:[^\]]+\]\] \(" + talk + "\)")
-# e.g. 13:41, 9 Jun 2015 (UTC) -- the spaces after the comma and before the bracket are optional:
-timestampRe = re.compile(r"\d{2}:\d{2}, ?\d{1,2} (?i)(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Oct(ober)?|Nov(ember)?|Dec(ember)?) \d{4} {0,1}\(\w+\)")
-#                          \________/     \___/  \_______________________________________________________________________________________________________________/ \__/  \__________/
-#                            13:41   , ?    9                                                      Jun                                                             2015      (UTC)
+# e.g. 13:41, 9 Jun 2015 (UTC) -- the spaces after the comma and before the
+# bracket are optional. For simplicity reasons, we allow the month to appear
+# twice or not even once. Thus, incorrect timestamps are allowed but is highly unlikely.
+months = r' (?i)(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)'
+#           \_____________________________________________________________________________________________________________/
+#                                                          Jun(e)
+timestampRe = re.compile(r"\d{2}:\d{2},%s? ?\d{1,2}%s? ?\d{4} ?\(\w+\)" % (months, months))
+#                          \________/  \m/   \___/ \m/   \_/     \____/
+#                            13:41    ,    ?   9         2015  ?  (UTC)
 
 def removeSignature(text):
     """ Removes different forms of signatures. This includes timestamps,
